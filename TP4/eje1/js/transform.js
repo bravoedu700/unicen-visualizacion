ec44807bbcc34b184854a1c9d069c68f416002f0
bravoedu <bravo_edu700@hotmail.com>
2017-10-11 16:05:19 -0300

    ejemplos de transformacion css

---
+++ TP4/eje1/js/transform.js
@@ -0,0 +1,8 @@
+var nuclic=1;
+function rotarLogo(g){
+  g=g*nuclic;
+  x=g*2;
+  y=g*3;
+  document.getElementById("logo").style.transform = "translate("+ x +"px,"+ y +"px) rotate("+ g +"deg)";
+  nuclic++;
+}
