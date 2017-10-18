2498fb7bfe9aa6e372fe4637301d122f086264fa
bravoedu700 <bravo_edu700@hotmail.com>
2017-10-18 12:59:48 -0300

    agrewgando parallax

---
+++ TP4/EntregaFinal/js/game.js
@@ -0,0 +1,12 @@
+function Game(){
+  this.p = p;
+}
+Game.prototype.init = function(){
+/*con set time out */
+}
+
+Game.prototype.update = function(){
+/*con set time out
+este va viendo que teclas aprieta
+*/
+}
